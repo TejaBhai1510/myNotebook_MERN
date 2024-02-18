bb6f41e0fbe87ac23bf0d803764af3a4988e915c
Tejas <teja.bhandvalkar@gmail.com>
2024-02-18 12:11:26 +0530

    Added Data Validation

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/"
+const mongoURI = "mongodb://localhost:27017/myNotebook"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res) => {
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/', [
+    body('name', 'Enter a valid Name').isLength({ length: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 charecters').isLength({ length: 5 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+    res.json({ error: "Mail Already Exists", message: err.message})})
 })
 
 module.exports = router
