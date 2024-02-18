2aa79317a78cf71cd030f96e9873e1806282bd03
Tejas <teja.bhandvalkar@gmail.com>
2024-02-18 12:44:50 +0530

    Created thunderclient collections to manage requests

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,22 +3,35 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-router.post('/', [
+// Create a user using: POST "/api/auth/createuser". No login required
+router.post('/createuser', [
     body('name', 'Enter a valid Name').isLength({ length: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 charecters').isLength({ length: 5 })
-], (req, res) => {
+
+], async (req, res) => {
+    // If there are errors, return Bad request along with the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-    res.json({ error: "Mail Already Exists", message: err.message})})
+    // Check whether a user exists with the email already
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists!" });
+        }
+        // Create a new user
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some Error occured")
+    }
 })
 
 module.exports = router
