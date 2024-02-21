801570c4e0cbf467abac496c9fc36add34f2b9ce
Tejas <teja.bhandvalkar@gmail.com>
2024-02-21 15:49:49 +0530

    Updation of existing Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,7 +4,7 @@ const fetchuser = require('../middleware/fetchuser');
 const { body, validationResult } = require('express-validator');
 const Note = require('../models/Note');
 
-// ROUTE 1: Get all notes using: GET "/api/auth/fetchallnotes", Login Required
+// ROUTE 1: Get all notes using: GET "/api/notes/fetchallnotes", Login Required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.user.id })
@@ -15,7 +15,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
     }
 })
 
-// ROUTE 2: Add notes using: POST "/api/auth/addnote", Login Required
+// ROUTE 2: Add notes using: POST "/api/notes/addnote", Login Required
 router.post('/addnote', fetchuser, [
     body('title', 'Enter a valid title').isLength({ length: 3 }),
     body('description', 'Description must be atleast 5 charecters').isLength({ length: 5 })
@@ -30,6 +30,33 @@ router.post('/addnote', fetchuser, [
         const note = new Note({ title, description, tag, user: req.user.id });
         const savedNote = await note.save();
         res.json(savedNote);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some Error occured")
+    }
+})
+
+// ROUTE 3: Updating an existing Note using: PUT "/api/notes/updatenote", Login Required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    try {
+        const {title, description, tag} = req.body;
+        // Create a new note Object
+        const newNote = {};
+        if(title){newNote.title = title};
+        if(description){newNote.description = description};
+        if(tag){newNote.tag = tag};
+
+        // Find the note to be Updated & Update it
+        let note = await Note.findById(req.params.id);
+        if(!note){
+            return res.status(404).send("Not Found");
+        }
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Authorized");
+        }
+
+        note = await Note.findOneAndUpdate(req.params.id, {$set: newNote}, {new: true});
+        res.json({note});
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some Error occured")
