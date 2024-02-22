e10cc04962ce4d04f8f225823a4ed26d3c80c332
Tejas <teja.bhandvalkar@gmail.com>
2024-02-22 12:50:20 +0530

    used Context API: useContext hook

--- src/App.js
+++ src/App.js
@@ -8,22 +8,25 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Home />
-        <Routes>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Home />
+          <Routes>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
