99685ab8c4e5e33727f7328c4eb56d1d47a1d934
Tejas <teja.bhandvalkar@gmail.com>
2024-02-23 19:14:26 +0530

    Fetched notes from Context

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,8 +1,12 @@
 import NoteContext from "./noteContext";
+import { useState } from "react";
 
 const NoteState = (props) => {
+    const notesInitial = [];
+    const [notes, setNotes] = useState(notesInitial);
+
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             { props.children }
         </NoteContext.Provider>
     )
